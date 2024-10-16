d628d0b200a7bb422adbc38edce7379a276ec802
Snel <fransnellagirousse@gmail.com>
2024-10-16 22:08:31 +0300

    added an edit in request in advance and navigation

--- src/ui/components/forms/request_in_advance/amountAdvanceForm.tsx
+++ src/ui/components/forms/request_in_advance/amountAdvanceForm.tsx
@@ -5,175 +5,221 @@ import { Controller, useFieldArray } from "react-hook-form";
 import { AdvanceInput } from "@/ui/design-system/forms/AdvanceInput";
 import CurrencyInput from "react-currency-input-field";
 import { Button } from "@/ui/design-system/button/button";
+import { MdDeleteForever } from "react-icons/md";
+import { useEffect, useState } from "react";
 
 interface Props {
   form: FormsType;
 }
 
 export const AmountAdvanceForm = ({ form }: Props) => {
-  const { control, isLoading, register, errors } = form;
+  const { control, isLoading, register, errors, watch } = form;
   const { fields, append, remove } = useFieldArray({
     control,
     name: "rows",
   });
+
+  const [totalSum, setTotalSum] = useState(0);
+
+  
+  useEffect(() => {
+    const totals = fields.map((_, index) => {
+      const perDiemRate = watch(`rows[${index}].per_diem_rate`) || 0;
+      const percentageOfAdvance = watch(`rows[${index}].percentage_of_advance_required`) || 0;
+      const numberOfDays = watch(`rows[${index}].number_of_days`) || 0;
+
+      const total =
+        (parseFloat(perDiemRate) *
+          parseFloat(percentageOfAdvance) *
+          parseFloat(numberOfDays)) /
+        100;
+      return isNaN(total) ? 0 : total;
+    });
+
+    const sum = totals.reduce((acc, curr) => acc + curr, 0);
+    setTotalSum(sum); 
+  }, [fields, watch]); 
+
   return (
-    <>
-      <table>
+    <div className="overflow-x-auto">
+      <table className="table-auto border-collapse border border-gray-400 w-full">
         <thead>
           <tr>
-            <th>Location</th>
-            <th>Taux journalier</th>
-            <th>Pourcentage de l'avance nécessaire</th>
-            <th>Coefficient d'évaluation journalier</th>
-            <th>Total</th>
+            <th className="border border-gray-400 p-2 break-words whitespace-normal">
+              Location
+            </th>
+            <th className="border border-gray-400 p-2 break-words whitespace-normal">
+              Taux journalier
+            </th>
+            <th className="border border-gray-400 p-2 break-words whitespace-normal">
+              Pourcentage de l'avance nécessaire
+            </th>
+            <th className="border border-gray-400 p-2 break-words whitespace-normal">
+              Coefficient d'évaluation journalier
+            </th>
+            <th className="border border-gray-400 p-2 break-words whitespace-normal">
+              Total
+            </th>
+            <th className="border border-gray-400 p-2">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {fields.map((item, index) => (
-            <tr key={item.id}>
-              <td>
-                <AdvanceInput
-                  type="text"
-                  id="location"
-                  placeholder="Date: Location ..."
-                  isLoading={isLoading}
-                  register={register}
-                  errors={errors}
-                />
-              </td>
-              <td>
-                <Controller
-                  name="per_diem_rate"
-                  control={control}
-                  rules={{
-                    required: "Ce champ est requis",
-                    min: {
-                      value: 0,
-                      message: "Ce champ doit être supérieur à 0",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <CurrencyInput
-                      id="per_diem_rate"
-                      name="per_diem_rate"
-                      decimalScale={2}
-                      prefix="Ariary"
-                      placeholder="Ariary 100, 000"
-                      value={field.value}
-                      onValueChange={(value) => field.onChange(value)}
-                      allowNegativeValue={false}
-                      className="bg-white outline-0 border-b border-gray-500 text-gray p-3"
-                    />
-                  )}
-                />
-              </td>
-              <td>
-                <Controller
-                  name="percentage_of_advance_required"
-                  control={control}
-                  rules={{
-                    required: "Ce champ est requis",
-                    min: {
-                      value: 0,
-                      message: "Percentage must be between 0 and 100",
-                    },
-                    max: {
-                      value: 100,
-                      message: "Percentage must be between 0 and 100",
-                    },
-                    pattern: {
-                      value: /^[0-9]*\.?[0-9]{0,2}$/,
-                      message: "Invalid percentage format",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <CurrencyInput
-                      id="percentage_of_advance_required"
-                      name="percentage_of_advance_required"
-                      decimalScale={2}
-                      prefix=""
-                      suffix="%"
-                      placeholder="10 %"
-                      value={field.value}
-                      groupSeparator=""
-                      onValueChange={(value) => field.onChange(value)}
-                      allowNegativeValue={false}
-                      maxLength={5}
-                      decimalsLimit={2}
-                      className="bg-white outline-0 border-b border-gray-500 text-gray p-3" // Limite le nombre de décimales
-                    />
-                  )}
-                />
-              </td>
-              <td>
-                <Controller
-                  name={`rows[${index}].number_of_days`}
-                  control={control}
-                  rules={{
-                    required: "Ce champ est requis",
-                    min: {
-                      value: 0,
-                      message:
-                        "Le nombre de jours doit être supérieur ou égal à 0",
-                    },
-                    max: {
-                      value: 100,
-                      message:
-                        "Le nombre de jours doit être inférieur ou égal à 100",
-                    },
-                    pattern: {
-                      value: /^[0-9]*\.?[0-9]{0,2}$/,
-                      message: "Format invalide",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <CurrencyInput
-                      id={`number_of_days-${index}`}
-                      name={`rows[${index}].number_of_days`}
-                      decimalScale={2}
-                      placeholder="0,75"
-                      value={field.value}
-                      onValueChange={(value) => field.onChange(value)}
-                      allowNegativeValue={false}
-                      className="bg-white outline-0 border-b border-gray-500 text-gray p-3"
-                    />
-                  )}
-                />
-              </td>
-              <td>
-                <Controller
-                  name="total_amount"
-                  control={control}
-                  rules={{
-                    required: "Ce champ est requis",
-                    min: {
-                      value: 0,
-                      message: "Ce champ doit être supérieur à 0",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <CurrencyInput
-                      id="total_amount"
-                      name="total_amount"
-                      decimalScale={2}
-                      prefix="Ariary"
-                      placeholder="Ariary 100, 000"
-                      value={field.value}
-                      onValueChange={(value) => field.onChange(value)}
-                      allowNegativeValue={false}
-                      className="bg-white outline-0 border-b border-gray-500 text-gray p-3"
-                    />
-                  )}
-                />
-              </td>
-              <td>
-                <button type="button" onClick={() => remove(index)}>
-                  Supprimer
-                </button>
-              </td>
-            </tr>
-          ))}
+          {fields.map((item, index) => {
+            const perDiemRate = watch(`rows[${index}].per_diem_rate`) || 0;
+            const percentageOfAdvance =
+              watch(`rows[${index}].percentage_of_advance_required`) || 0;
+            const numberOfDays = watch(`rows[${index}].number_of_days`) || 0;
+
+            const total =
+              (parseFloat(perDiemRate) *
+                parseFloat(percentageOfAdvance) *
+                parseFloat(numberOfDays)) /
+              100;
+
+            return (
+              <tr key={item.id} className="text-sm md:text-base">
+                <td className="border border-gray-400 p-2 break-words whitespace-normal">
+                  <AdvanceInput
+                    type="text"
+                    id={`rows[${index}].location`}
+                    placeholder="Date: Location ..."
+                    isLoading={isLoading}
+                    register={register}
+                    errors={errors}
+                  />
+                </td>
+                <td className="border border-gray-400 p-2 w-[200px] break-words whitespace-normal">
+                  <Controller
+                    name={`rows[${index}].per_diem_rate`}
+                    control={control}
+                    rules={{
+                      required: "Ce champ est requis",
+                      min: {
+                        value: 0,
+                        message: "Ce champ doit être supérieur à 0",
+                      },
+                    }}
+                    render={({ field }) => (
+                      <CurrencyInput
+                        id={`rows[${index}].per_diem_rate`}
+                        decimalScale={2}
+                        prefix="Ariary"
+                        placeholder="Ariary 100, 000"
+                        value={field.value}
+                        onValueChange={(value) => field.onChange(value)}
+                        allowNegativeValue={false}
+                        className="bg-white outline-0 border border-gray-400 p-2 w-full"
+                      />
+                    )}
+                  />
+                </td>
+                <td className="border border-gray-400 p-2 w-[100px] break-words whitespace-normal">
+                  <Controller
+                    name={`rows[${index}].percentage_of_advance_required`}
+                    control={control}
+                    rules={{
+                      required: "Ce champ est requis",
+                      min: {
+                        value: 0,
+                        message: "Percentage must be between 0 and 100",
+                      },
+                      max: {
+                        value: 100,
+                        message: "Percentage must be between 0 and 100",
+                      },
+                      pattern: {
+                        value: /^[0-9]*\.?[0-9]{0,2}$/,
+                        message: "Invalid percentage format",
+                      },
+                    }}
+                    render={({ field }) => (
+                      <CurrencyInput
+                        id={`rows[${index}].percentage_of_advance_required`}
+                        decimalScale={2}
+                        suffix="%"
+                        placeholder="10 %"
+                        value={field.value}
+                        onValueChange={(value) => field.onChange(value)}
+                        allowNegativeValue={false}
+                        className="bg-white outline-0 border border-gray-400 p-2 w-full"
+                      />
+                    )}
+                  />
+                </td>
+                <td className="border border-gray-400 p-2 w-[100px] break-words whitespace-normal">
+                  <Controller
+                    name={`rows[${index}].number_of_days`}
+                    control={control}
+                    rules={{
+                      required: "Ce champ est requis",
+                      min: {
+                        value: 0,
+                        message:
+                          "Le nombre de jours doit être supérieur ou égal à 0",
+                      },
+                      max: {
+                        value: 100,
+                        message:
+                          "Le nombre de jours doit être inférieur ou égal à 100",
+                      },
+                      pattern: {
+                        value: /^[0-9]*\.?[0-9]{0,2}$/,
+                        message: "Format invalide",
+                      },
+                    }}
+                    render={({ field }) => (
+                      <CurrencyInput
+                        id={`rows[${index}].number_of_days`}
+                        decimalScale={2}
+                        placeholder="0,75"
+                        value={field.value}
+                        onValueChange={(value) => field.onChange(value)}
+                        allowNegativeValue={false}
+                        className="bg-white outline-0 border border-gray-400 p-2 w-full"
+                      />
+                    )}
+                  />
+                </td>
+                <td className="border border-gray-400 p-2 w-[200px] break-words whitespace-normal">
+                  <CurrencyInput
+                    id={`rows[${index}].total_amount`}
+                    name={`rows[${index}].total_amount`}
+                    value={total}
+                    prefix="Ariary"
+                    decimalScale={2}
+                    disabled={true}
+                    className="bg-white outline-0 border border-gray-400 p-2 w-full"
+                  />
+                </td>
+                <td className="border border-gray-400 p-2 w-[100px]">
+                  <div className="flex space-x-2">
+                    <button type="button" onClick={() => remove(index)}>
+                      <MdDeleteForever className="text-red-500" size={24} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4} className="text-right font-bold p-2">
+              Total général:
+            </td>
+            <td className="border border-gray-400 p-2">
+              <CurrencyInput
+                id="total_sum"
+                value={totalSum}
+                prefix="Ariary"
+                decimalScale={2}
+                disabled={true}
+                className="bg-white outline-0 border border-gray-400 p-2 w-full font-bold"
+              />
+            </td>
+            <td></td>
+          </tr>
+        </tfoot>
       </table>
 
       <Button>
@@ -182,16 +228,16 @@ export const AmountAdvanceForm = ({ form }: Props) => {
           onClick={() =>
             append({
               location: "",
-              perDiemRate: "",
-              numberOfDays: "",
-              percentage: "",
-              total: "",
+              per_diem_rate: "",
+              percentage_of_advance_required: "",
+              number_of_days: "",
+              total_amount: "",
             })
           }
         >
           Ajouter une ligne
         </button>
       </Button>
-    </>
+    </div>
   );
 };
--- src/ui/components/navigation/navigation.tsx
+++ src/ui/components/navigation/navigation.tsx
@@ -1,108 +1,150 @@
-"use client"
+"use client";
 
-import {Container} from "@/ui/components/container/container"
-import {Logo} from "@/ui/design-system/logo/logo";
-import {Typography} from "@/ui/design-system/typography/typography";
-import {Button} from "@/ui/design-system/button/button";
+import { Container } from "@/ui/components/container/container";
+import { Logo } from "@/ui/design-system/logo/logo";
+import { Typography } from "@/ui/design-system/typography/typography";
+import { Button } from "@/ui/design-system/button/button";
 import Link from "next/link";
 import { RxHamburgerMenu } from "react-icons/rx";
-import {useState} from "react";
+import { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md"
-import {Sidebar} from "@/ui/components/navigation/sidebar";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { Sidebar } from "@/ui/components/navigation/sidebar";
 import clsx from "clsx";
-import {signOut, useSession} from "next-auth/react";
-import { IoPowerSharp } from "react-icons/io5"
-import {Avatar} from "@/ui/design-system/avatar/avatar";
+import { signOut, useSession } from "next-auth/react";
+import { IoPowerSharp } from "react-icons/io5";
+import { IoMdSearch } from "react-icons/io";
+import { Avatar } from "@/ui/design-system/avatar/avatar";
 
-interface Props {
-}
+interface Props {}
 
 export const Navigation = ({}: Props) => {
-    const [show, setShow] = useState(false)
-    const [showingNav, setShowingNav] = useState(false)
-    const {data:session} = useSession()
-    const handleClick = () => {
-        setShow((show) => !show)
-    }
-    const showNav  = () => {
-        setShowingNav((show) => !show)
-    }
-    return (
-        <section>
-            <div className="border-b-2 border-gray-400">
-                <Container className="flex items-center justify-between py-1.5 gap-7">
-                    <div className="flex items-center gap-5 relative">
-                        <div className="z-20">
-                            <button
-                                type="button"
-                                className={clsx(show && "fixed top-[16.4px]","border-2 border-gray text-gray rounded flex items-center justify-center w-[40px] h-[40px]")}
-                                onClick={handleClick}
-                            >
-                                {show ?  <IoCloseSharp/> : <RxHamburgerMenu />}
-                            </button>
-                        </div>
-                        <Link href="/">
-                            <div className="flex items-center gap-2.5">
-                                <Logo size="small"/>
-                                <div className="flex flex-col max-sm:hidden">
-                                    <div className="text-gray font-extrabold text-[24px]">
-                                        Gestion Financières
-                                    </div>
-                                    <Typography
-                                        variant="caption4"
-                                        tag="span"
-                                        theme="gray"
-                                    >
-                                        Votre solution financière !
-                                    </Typography>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    {session ?
-                        <div className="relative">
-                            <div className="flex justify-between gap-1 items-center">
-                                <Avatar size="medium" src={session?.user?.image} alt={session?.user?.name} />
-                                <span className="text-2xl" onClick={showNav}>
-                                    <MdOutlineKeyboardArrowDown/>
-                                </span>
-                            </div>
-                            <div className={clsx("absolute w-[200px] bg-white p-5 right-0 top-10 shadow-2xl rounded", !showingNav ? "hidden" : "block")}>
-                                <Typography
-                                    variant="caption3"
-                                    tag="span"
-                                    theme="black"
-                                >
-                                    <button onClick={() => signOut()} className="flex justify-between gap-2 items-center text-gray hover:text-primary transition-all">
-                                        <IoPowerSharp/>
-                                        Déconnecter
-                                    </button>
-                                </Typography>
-                            </div>
-                        </div>
-                        :
-                        <div className="flex items-center gap-7">
-                            <div className="flex items-center gap-2">
-                                <Button
-                                    baseUrl="/login"
-                                    size="small"
-                                >
-                                    Connexion
-                                </Button>
-                                <Button
-                                    size="small"
-                                    variant="outline"
-                                    baseUrl="/register"
-                                >
-                                    Réjoindre
-                                </Button>
-                            </div>
-                        </div>
-                    }
-                </Container>
+  const [show, setShow] = useState(false);
+  const [showingNav, setShowingNav] = useState(false);
+  const [searchQuery, setSearchQuery] = useState(""); // État pour la recherche
+  const { data: session } = useSession(); // Obtenir la session
+
+  const handleClick = () => {
+    setShow((show) => !show);
+  };
+
+  const showNav = () => {
+    setShowingNav((show) => !show);
+  };
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Search for:", searchQuery);
+    // Logique de recherche à implémenter ici
+  };
+
+  return (
+    <section>
+      <div className="border-b-2 border-gray-400">
+        <Container className="flex items-center justify-between py-1.5 gap-7">
+          <div className="flex items-center gap-5 relative">
+            <div className="z-20">
+              <button
+                type="button"
+                className={clsx(
+                  show && "fixed top-[16.4px]",
+                  "border-2 border-gray text-gray rounded flex items-center justify-center w-[40px] h-[40px]"
+                )}
+                onClick={handleClick}
+              >
+                {show ? <IoCloseSharp /> : <RxHamburgerMenu />}
+              </button>
             </div>
-            <Sidebar show={show}/>
-        </section>
-    )
-}
+            <Link href="/">
+              <div className="flex items-center gap-2.5">
+                <Logo size="small" />
+                <div className="flex flex-col max-sm:hidden">
+                  <div className="text-gray font-extrabold text-[24px]">
+                    Gestion Financières
+                  </div>
+                  <Typography variant="caption4" tag="span" theme="gray">
+                    Votre solution financière !
+                  </Typography>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+          {session && (
+            <form
+              onSubmit={handleSearch}
+              className="flex items-center w-[40%] max-w-md gap-2"
+            >
+              <input
+                type="text"
+                placeholder="Rechercher..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="border border-gray-300 rounded-l-full py-2 px-4 w-full outline-none"
+              />
+              <button
+                type="submit"
+                className="bg-primary-400 hover:bg-gray-300 rounded-r-full py-2 px-4 flex items-center justify-center"
+              >
+                <IoMdSearch className="text-gray-600" />
+              </button>
+            </form>
+          )}
+
+          {session ? (
+            <div className="relative">
+              <div className="flex justify-between gap-1 items-center">
+                <Avatar
+                  size="medium"
+                  src={session?.user?.image}
+                  alt={session?.user?.name}
+                />
+                <span className="text-2xl" onClick={showNav}>
+                  <MdOutlineKeyboardArrowDown />
+                </span>
+              </div>
+              <div
+                className={clsx(
+                  "absolute w-[200px] bg-white p-5 right-0 top-10 shadow-2xl rounded",
+                  !showingNav ? "hidden" : "block"
+                )}
+              >
+                <Typography variant="caption3" tag="span" theme="black">
+                  <Link
+                    href="/profile"
+                    className="flex justify-between gap-2
+                    items-center text-gray hover:text-primary transition-all
+                    mb-3"
+                  >
+                    <MdOutlineAccountCircle />
+                    Mon compte
+                  </Link>
+                  <button
+                    onClick={() => signOut()}
+                    className="flex justify-between gap-2 items-center text-gray hover:text-primary transition-all"
+                  >
+                    <IoPowerSharp />
+                    Déconnecter
+                  </button>
+                </Typography>
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center gap-7">
+              <div className="flex items-center gap-2">
+                <Button baseUrl="/login" size="small">
+                  Connexion
+                </Button>
+                <Button size="small" variant="outline" baseUrl="/register">
+                  Réjoindre
+                </Button>
+              </div>
+            </div>
+          )}
+        </Container>
+      </div>
+      <Sidebar show={show} />
+    </section>
+  );
+};
