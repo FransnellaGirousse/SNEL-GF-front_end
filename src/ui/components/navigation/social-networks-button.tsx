1200849855bc8f7746774411fbe40eb061b8daec
Snel <fransnellagirousse@gmail.com>
2024-11-11 22:10:26 +0300

    modification in network and home

--- src/ui/components/navigation/social-networks-button.tsx
+++ src/ui/components/navigation/social-networks-button.tsx
@@ -1,26 +1,40 @@
-import {footerNetworkLinks} from "@/src/ui/components/navigation/app-links";
-import {v4 as uuidv4} from "uuid";
-import {Button} from "@/src/ui/design-system/button/button";
+import { footerNetworkLinks } from "@/src/ui/components/navigation/app-links";
+import { v4 as uuidv4 } from "uuid";
+import { Button } from "@/src/ui/design-system/button/button";
 import clsx from "clsx";
-import {RiFacebookBoxFill} from "react-icons/ri";
+import { RiFacebookBoxFill } from "react-icons/ri";
+
+
+interface SocialNetwork {
+  icon?: React.ComponentType; // or use a more specific type if available
+  baseUrl: string;
+  type: string;
+}
 
 interface Props {
-    theme?: "gray" | "accent" | "secondary",
-    className?: string
+  theme?: "gray" | "accent" | "secondary";
+  className?: string;
 }
 
-export const SocialNetworksButtons = ({className, theme = "accent"}: Props) => {
-    const icoList = footerNetworkLinks.map((socialNetwork) => (
-        <Button
-            key={uuidv4()}
-            variant="ico"
-            iconTheme={theme}
-            icon={{icon: socialNetwork.icon ? socialNetwork.icon : RiFacebookBoxFill}}
-            baseUrl={socialNetwork.baseUrl}
-            linkType={socialNetwork.type}
-        />
-    ))
-    return (
-        <div className={clsx(className, "flex items-center gap-2.5")}>{icoList}</div>
-    )
-}
+export const SocialNetworksButtons = ({
+  className,
+  theme = "accent",
+}: Props) => {
+  const icoList = footerNetworkLinks.map((socialNetwork: SocialNetwork) => (
+    <Button
+      key={uuidv4()}
+      variant="ico"
+      iconTheme={theme}
+      icon={{
+        icon: socialNetwork.icon ? socialNetwork.icon : RiFacebookBoxFill,
+      }}
+      baseUrl={socialNetwork.baseUrl}
+      linkType={socialNetwork.type}
+    />
+  ));
+  return (
+    <div className={clsx(className, "flex items-center gap-2.5")}>
+      {icoList}
+    </div>
+  );
+};
--- src/ui/modules/home/components/acceuilComponents.tsx
+++ src/ui/modules/home/components/acceuilComponents.tsx
@@ -1,28 +1,33 @@
-
+import { Typography } from "@/ui/design-system/typography/typography";
 
 export const AcceuilComponents = () => {
-    return (
-      <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-6xl p-8 rounded-lg shadow-lg">
-        <div className="text-black mb-6 md:mb-0 md:mr-6 flex-1">
-          <h1 className="text-4xl font-bold mb-4">
-            Bienvenue sur Snell - Simplifiez la gestion de
-            vos finances
-          </h1>
-          <p className="text-lg mb-4 ">
-            Notre plateforme vous permet de gérer vos comptes en toute
-            simplicité, d'accéder à des rapports détaillés, et d'optimiser vos
-            dépenses. Que vous soyez un particulier ou une entreprise, nous
-            avons les outils pour vous aider à atteindre vos objectifs
-            financiers.
-          </p>
-        </div>
-        <div className="flex-1">
-          <img
-            src="assets/images/home/finance-image.jpeg"
-            alt="Gestion Financière"
-            className="w-full h-auto"
-          />
-        </div>
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-6xl p-8 rounded-lg shadow-lg">
+      <div className="text-black mb-6 md:mb-0 md:mr-6 flex-1">
+        <Typography
+          variant="h5" 
+          theme="black"
+        >
+          Bienvenue sur FinanSnell - Simplifiez la gestion de vos finances
+        </Typography>
+
+        <Typography 
+        variant="caption2" 
+        tag="p"
+        theme="black">
+          Notre plateforme vous permet de gérer vos comptes en toute simplicité,
+          d'accéder à des rapports détaillés, et d'optimiser vos dépenses. Que
+          vous soyez un particulier ou une entreprise, nous avons les outils
+          pour vous aider à atteindre vos objectifs financiers.
+        </Typography>
       </div>
-    );
-}
+      <div className="flex-1">
+        <img
+          src="/assets/images/home/finance-image.jpeg" 
+          alt="Gestion Financière"
+          className="w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+};
