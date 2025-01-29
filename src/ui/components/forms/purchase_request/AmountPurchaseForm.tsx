1eb58f55a1daec821e6ac73fcd55c2d3437cfbe0
Snel <fransnellagirousse@gmail.com>
2025-01-29 21:39:01 +0300

    table chect in Purchase request

--- src/ui/components/forms/purchase_request/AmountPurchaseForm.tsx
+++ src/ui/components/forms/purchase_request/AmountPurchaseForm.tsx
@@ -1,140 +1,132 @@
 "use client";
 
 import { FormsType } from "@/types/forms";
-import { useFieldArray, useFormContext } from "react-hook-form";
-import { Button } from "@/ui/design-system/button/button";
+import { Typography } from "@/ui/design-system/typography/typography";
 import { MdDeleteForever } from "react-icons/md";
+import {  useFieldArray } from "react-hook-form";
 import { Table } from "@/ui/design-system/table/table";
-import { Input } from "@/ui/design-system/forms/input";
+import { Button } from "@/ui/design-system/button/button";
+import { AdvanceInput } from "@/ui/design-system/forms/AdvanceInput";
 
 interface Props {
   form: FormsType;
 }
 
 export const AmountPurchaseForm = ({ form }: Props) => {
-  const { control, isLoading, register, errors, watch } = form;
+  const { control, isLoading, register, errors } = form;
   const { fields, append, remove } = useFieldArray({
     control,
     name: "rows",
   });
 
-  // Définition des colonnes pour le tableau
   const columns = [
-    { title: "Article", key: "item" },
-    { title: "Quantité", key: "quantity" },
+    { title: "Date", key: "date_hour" },
+    { title: "Quantité", key: "item" },
     { title: "Type d'unité", key: "unit_type" },
-    { title: "Description", key: "description" },
+    { title: "Destination", key: "destination" },
     { title: "Prix d'unité estimé", key: "estimated_unit_price" },
     { title: "Total estimé", key: "estimated_total" },
     { title: "Notes", key: "notes" },
     { title: "Actions", key: "actions" },
   ];
 
-  // Génération des lignes du tableau
-  const rows = fields.map((item, index) => {
-    const quantity = parseFloat(watch(`rows[${index}].quantity`) || "0");
-    const estimatedUnitPrice = parseFloat(
-      watch(`rows[${index}].estimated_unit_price`) || "0"
-    );
-    const estimatedTotal = (quantity * estimatedUnitPrice).toFixed(2);
-
-    return {
-      id: item.id,
-      data: {
-        item: (
-          <Input
-            type="text"
-            id={`rows[${index}].item`}
-            placeholder="Article"
-            isLoading={isLoading}
-            register={register}
-            errors={errors}
-          />
-        ),
-        quantity: (
-          <Input
-            type="number"
-            id={`rows[${index}].quantity`}
-            placeholder="Quantité"
-            isLoading={isLoading}
-            register={register}
-            errors={errors}
-          />
-        ),
-        unit_type: (
-          <Input
-            type="text"
-            id={`rows[${index}].unit_type`}
-            placeholder="Type d'unité"
-            isLoading={isLoading}
-            register={register}
-            errors={errors}
-          />
-        ),
-        description: (
-          <Input
-            type="text"
-            id={`rows[${index}].description`}
-            placeholder="Description"
-            isLoading={isLoading}
-            register={register}
-            errors={errors}
-          />
-        ),
-        estimated_unit_price: (
-          <Input
-            type="number"
-            id={`rows[${index}].estimated_unit_price`}
-            placeholder="Prix d'unité estimé"
-            isLoading={isLoading}
-            register={register}
-            errors={errors}
-          />
-        ),
-        estimated_total: (
-          <Input
-            type="text"
-            id={`rows[${index}].estimated_total`}
-            value={estimatedTotal} // Calcul automatique
-            placeholder="Total estimé"
-            isLoading={isLoading}
-            disabled={true} // Non modifiable
-          />
-        ),
-        notes: (
-          <Input
-            type="text"
-            id={`rows[${index}].notes`}
-            placeholder="Notes"
-            isLoading={isLoading}
-            register={register}
-            errors={errors}
-          />
-        ),
-        actions: (
-          <button type="button" onClick={() => remove(index)}>
-            <MdDeleteForever className="text-red-500" size={20} />
-          </button>
-        ),
-      },
-    };
-  });
+  const rows = fields.map((item, index) => ({
+    id: item.id,
+    data: {
+      date_hour: (
+        <AdvanceInput
+          type="date"
+          id={`rows[${index}].date_hour`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      item: (
+        <AdvanceInput
+          type="text"
+          placeholder="Quantité"
+          id={`rows[${index}].item`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      unit_type: (
+        <AdvanceInput
+          type="text"
+          placeholder="Type d'unité"
+          id={`rows[${index}].unit_type`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      destination: (
+        <AdvanceInput
+          type="text"
+          placeholder="Destination"
+          id={`rows[${index}].destination`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      estimated_unit_price: (
+        <AdvanceInput
+          type="text"
+          placeholder="Prix d'unité estimé"
+          id={`rows[${index}].estimated_unit_price`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      estimated_total: (
+        <AdvanceInput
+          type="text"
+          placeholder="Total estimé"
+          id={`rows[${index}].estimated_total`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      notes: (
+        <AdvanceInput
+          type="text"
+          placeholder="Notes"
+          id={`rows[${index}].notes`}
+          isLoading={isLoading}
+          register={register}
+          errors={errors}
+        />
+      ),
+      actions: (
+        <button type="button" onClick={() => remove(index)}>
+          <MdDeleteForever className="text-red-500" size={20} />
+        </button>
+      ),
+    },
+  }));
 
   return (
-    <div className="overflow-x-auto">
-      {/* Tableau des articles */}
+    <div className="p-4">
+      <Typography variant="lead" tag="h5" theme="black" className="text-center">
+        Details de la demande
+      </Typography>
+
       <Table columns={columns} rows={rows} />
 
-      {/* Bouton pour ajouter une ligne */}
       <Button>
         <button
           type="button"
           onClick={() =>
             append({
+              date_hour: "",
               item: "",
-              quantity: "",
               unit_type: "",
-              description: "",
+              destination: "",
               estimated_unit_price: "",
               estimated_total: "",
               notes: "",
